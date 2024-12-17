e24d8bdd893c3abaffa2a2d66a6006b6889dae06
hadyawayda <hady_awayda@hotmail.com>
2024-12-18 00:02:23 +0200

    Updated settings icon with proper styling and adjusted its pressable box

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,15 +1,17 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
+import SettingsIcon from '../components/SettingsIcon';
 import { MainScreenProps } from '../types/screens';
 
 const MainScreen = ({ navigation }: MainScreenProps) => {
   return (
     <View style={styles.container}>
-      <Button
-        title="Settings"
+      <TouchableOpacity
+        style={styles.settingsButton}
         onPress={() =>
           navigation.navigate('SettingsStack', { screen: 'SettingsScreen' })
-        }
-      />
+        }>
+        <SettingsIcon />
+      </TouchableOpacity>
       <Button
         title="Launch Voicebot"
         onPress={() => navigation.navigate('VoicebotScreen')}
@@ -21,6 +23,17 @@ const MainScreen = ({ navigation }: MainScreenProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgray',
+  },
+  settingsButton: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    top: 10,
+    left: 10,
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
